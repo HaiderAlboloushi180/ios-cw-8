6bc5dbb4b0b08bf9950a81a0eb8f849ad90525ea
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-07-08 20:17:17 +0300

    finished cw

---
+++ IOSClasswork8/iOSClasswork8/ViewController/CityCell.swift
@@ -0,0 +1,16 @@
+//
+//  CityCell.swift
+//  iOSClaswork8
+//
+//  Created by Fatimah Alhashem on 7/8/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var cardLabel: UILabel!
+    
+}
